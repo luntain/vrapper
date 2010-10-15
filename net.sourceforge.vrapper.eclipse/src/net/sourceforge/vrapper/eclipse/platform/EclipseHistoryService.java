b9e9f31c2e15ea558ce0ec1f8746799cf70b3e5d
Kamil Dworakowski <kamil.dworakowski@youdevise.com>
2010-10-15 12:06:30 +0100

    some instrumentation and blocking disconnect
    blocking the disconnect prevents breaking of undo after
    renaming a java class
    
    Looks like Eclipse is creating
    a new UndoManager after renaming a class and disconects
    from the old one (the one wrapped by us).

--- net.sourceforge.vrapper.eclipse/src/net/sourceforge/vrapper/eclipse/platform/EclipseHistoryService.java
+++ net.sourceforge.vrapper.eclipse/src/net/sourceforge/vrapper/eclipse/platform/EclipseHistoryService.java
@@ -1,5 +1,8 @@
 package net.sourceforge.vrapper.eclipse.platform;
 
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+
 import net.sourceforge.vrapper.platform.HistoryService;
 
 import org.eclipse.core.commands.operations.IUndoContext;
@@ -9,68 +12,96 @@ import org.eclipse.jface.text.IUndoManagerExtension;
 import org.eclipse.swt.custom.StyledText;
 
 public class EclipseHistoryService implements IUndoManager, IUndoManagerExtension, HistoryService {
+	private static final Logger LOGGER = Logger.getLogger(EclipseHistoryService.class.getName());
 
     private final IUndoManager delegate;
     private boolean locked;
     private final StyledText textWidget;
+    
+    static {
+    	FileHandler handler;
+		try {
+			handler = new FileHandler("/tmp/vrapper.log");
+			LOGGER.addHandler(handler);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    }
 
     public EclipseHistoryService(StyledText textWidget, IUndoManager delegate) {
         this.textWidget = textWidget;
         this.delegate = delegate;
+        LOGGER.info("Initialised EclipseHistoryService");
     }
 
     public void lock() {
+    	LOGGER.info("lock()");
         locked = true;
     }
 
     public void unlock() {
+    	LOGGER.info("unlock()");
         locked = false;
     }
 
     public void beginCompoundChange() {
+    	LOGGER.info("beginCompoundChange()");
         if (!locked) {
+        	LOGGER.info("delegate.beginCompoundChange()");
             delegate.beginCompoundChange();
         }
     }
 
     public void endCompoundChange() {
+    	LOGGER.info("endCompoundChange()");
         if (!locked) {
+        	LOGGER.info("delegate.endCompoundChange()");
             delegate.endCompoundChange();
         }
     }
 
     public void connect(ITextViewer arg0) {
+    	LOGGER.info("connect()");
         delegate.connect(arg0);
     }
 
     public void disconnect() {
-        delegate.disconnect();
+    	LOGGER.info("disconnect()");
+        //delegate.disconnect();
     }
 
     public void undo() {
+    	LOGGER.info("undo()");
         delegate.undo();
         deselectAll();
     }
 
     public void redo() {
+    	LOGGER.info("redo()");
         delegate.redo();
         deselectAll();
     }
 
     public boolean undoable() {
-        return delegate.undoable();
+    	boolean undoable = delegate.undoable();
+    	LOGGER.info("undoable() returns " + undoable);
+		return undoable;
     }
 
     public boolean redoable() {
-        return delegate.redoable();
+    	boolean redoable = delegate.redoable();
+    	LOGGER.info("redoable() returns " + redoable);
+		return redoable;
     }
 
     public void reset() {
+    	LOGGER.info("reset()");
         delegate.reset();
     }
 
-    public void setMaximalUndoLevel(int arg0) {
-        delegate.setMaximalUndoLevel(arg0);
+    public void setMaximalUndoLevel(int level) {
+    	LOGGER.info("setMaximalUndoLevel(" + level + ")");
+        delegate.setMaximalUndoLevel(level);
     }
 
     private void deselectAll() {
@@ -81,6 +112,7 @@ public class EclipseHistoryService implements IUndoManager, IUndoManagerExtensio
     }
 
 	public IUndoContext getUndoContext() {
+    	LOGGER.info("getUndoContext()");
 		if (delegate instanceof IUndoManagerExtension) {
 			return ((IUndoManagerExtension)delegate).getUndoContext();
 		}
